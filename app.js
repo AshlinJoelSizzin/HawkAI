0a3aaabc2ef48d02547ecfa214e385727bed708b
Ashlin Joel <ashlinjoel30sizzin@gmail.com>
2023-10-21 13:39:03 +0530

    veg and non-veg menu upgrades

--- app.js
+++ app.js
@@ -10,12 +10,26 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/menu", function (req, res) {
   res.render("menu.ejs");
+});
+
+app.get("/veg", function (req, res) {
+  res.render("meal.ejs");
+});
+
+app.get("/nonveg", function (req, res) {
+  res.render("meal.ejs");
+});
+
+app.get("/previous", function (req, res) {
+  res.redirect("/menu");
 });
 
 app.listen(3000, function () {
